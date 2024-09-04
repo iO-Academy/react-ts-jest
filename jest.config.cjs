99a1c10983645b524d9e298bb6de7c225c1a713a
Ash Coles <ashley@acoles.co.uk>
2024-09-04 13:56:01 +0100

    Tweaking jest setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,5 +4,6 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest",
         // Jest cannot deal with SVGs or CSS files, jest-transform-stub fixes that
         "\\.(css|scss|svg)$": "jest-transform-stub",
-    }
+    },
+    "setupFiles": ["<rootDir>/jest.setup.ts"]
 };
