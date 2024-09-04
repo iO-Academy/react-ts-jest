033ff127ba5d685e196f579251e5c0cabae6159b
Ash Coles <ashley@acoles.co.uk>
2024-09-04 11:27:49 +0100

    Setup tweaks and installing fetch mock

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,5 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest",
         // Jest cannot deal with SVGs or CSS files, jest-transform-stub fixes that
         "\\.(css|scss|svg)$": "jest-transform-stub",
-    },
-    // Loads the setup file
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    }
 };
