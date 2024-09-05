44168d47fdfb5e2c7813fb7a6adbf57661fd7a6f
Ash Coles <ashley@acoles.co.uk>
2024-09-05 13:38:51 +0100

    Adding extra image formats to jest config stubbing

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,8 +2,9 @@ module.exports = {
     testEnvironment: "jsdom",
     transform: {
         "^.+\\.tsx?$": "ts-jest",
-        // Jest cannot deal with SVGs or CSS files, jest-transform-stub fixes that
-        "\\.(css|scss|svg)$": "jest-transform-stub",
+        // Jest cannot deal with files that aren't ts/js
+        // jest-transform-stub fixes that
+        "\\.(css|scss|svg|jpg|jpeg|png)$": "jest-transform-stub",
     },
     "setupFiles": ["<rootDir>/jest.setup.ts"]
 };
